9e5d19a8a74c2189f9031908cf6f9ffba4cae63f
catdanah <xnxjr1219@naver.com>
2021-09-03 23:46:02 +0900

    Add trigger menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    // 트리거 메뉴
+    $('header a.trigger, header .nav-bg').click(function() {
+        $('header .nav-bg').toggle();
+        $('header a.trigger span').toggleClass('on');
+        $('header nav').toggleClass('on');
+        if ( $('header nav').hasClass('on') ) {
+            $('header nav').animate({right:'-15%'});                            
+        } else {
+            $('header nav').animate({right:'-100%'});
+        }
+    });
+    // 슬라이더
     const mySwiper = new Swiper('.swiper-container', {
         loop : true,
         pagination : {
@@ -9,9 +21,5 @@ $(document).ready(function() {
             nextEl : '.swiper-button-next',
             prevEl : '.swiper-button-prev',
         },
-        // 반응형일때
-        // breakpoints : {
-        //     
-        // }
     });
 });
